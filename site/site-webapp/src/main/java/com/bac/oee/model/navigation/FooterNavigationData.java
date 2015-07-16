ac2a945d7319c3a730639c9f2c61a20298ba4457
Jose Aleman <jose.aleman@prodigious.com>
2015-07-16 10:38:32 -0600

    Latest change

--- site/site-webapp/src/main/java/com/bac/oee/model/navigation/FooterNavigationData.java
+++
@@ -1,7 +0,0 @@
-package com.bac.oee.model.navigation;
-
-/**
- * The Class FooterNavigationData.
- */
-public class FooterNavigationData extends NavigationWrapper {
-}
--- site/site-webapp/src/main/java/com/bac/oee/model/navigation/UtilityNavigationData.java
+++
@@ -1,8 +0,0 @@
-package com.bac.oee.model.navigation;
-
-/**
- * The Class UtilityNavigationData.
- */
-public class UtilityNavigationData extends NavigationWrapper {
-
-}
